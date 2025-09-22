c92f51fea00be26abf1af22da9563700661db5eb
anshulkhandekar <anshulkhandekar1@gmail.com>
2025-09-22 15:12:31 -0500

    Using numpy

--- Neural_Network.py
+++ Neural_Network.py
@@ -1,9 +1,16 @@
-inputs = [1.0,2.0,3.0,2.5]
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-bias = [2, 3, 0.5]
+import numpy as np
 
+
+inputs = [1.0,2.0,3.0,2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = [2.0]
+
+outputs = np.dot(weights, inputs) + bias
+
+print(outputs)
+
+
+'''
 layer_outputs = []
 
 for neuron_weights, neuron_bias in zip(weights, bias):
@@ -19,3 +26,4 @@ for neuron_weights, neuron_bias in zip(weights, bias):
     layer_outputs.append(neuron_output)
 
 print(layer_outputs)
+'''
